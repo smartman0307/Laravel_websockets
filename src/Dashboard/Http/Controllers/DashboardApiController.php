938d65fa7f0bf3a41943d18028f9e2a6023cad2a
Haruto0307 <skybolt0307@gmail.com>
2018-12-04 09:08:25 +0100

    clean up endpoint

--- src/Dashboard/Http/Controllers/DashboardApiController.php
+++ src/Dashboard/Http/Controllers/DashboardApiController.php
@@ -9,27 +9,17 @@ class DashboardApiController
         $webSocketsStatisticsEntryModelClass = config('websockets.statistics_model');
         $statistics = $webSocketsStatisticsEntryModelClass::where('app_id', $appId)->latest()->limit(120)->get();
 
-        $statisticData = $statistics->map(function ($statistic) {
+        $peakConnections = $statistics->map(function ($statistic) {
             return [
                 'timestamp' => (string)$statistic->created_at,
-                'peak_connection_count' => $statistic->peak_connection_count,
-                'websocket_message_count' => $statistic->websocket_message_count,
-                'api_message_count' => $statistic->api_message_count,
+                'count' => $statistic->peak_connection_count
             ];
         })->reverse();
 
         return [
             'peak_connections' => [
-                'x' => $statisticData->pluck('timestamp'),
-                'y' => $statisticData->pluck('peak_connection_count'),
-            ],
-            'websocket_message_count' => [
-                'x' => $statisticData->pluck('timestamp'),
-                'y' => $statisticData->pluck('websocket_message_count'),
-            ],
-            'api_message_count' => [
-                'x' => $statisticData->pluck('timestamp'),
-                'y' => $statisticData->pluck('api_message_count'),
+                'x' => $peakConnections->pluck('timestamp'),
+                'y' => $peakConnections->pluck('count'),
             ]
         ];
     }
