89db5546ee5f1fb49c3e6861d3c80fd3ba3aee8e
Haruto0307 <skybolt0307@gmail.com>
2020-08-24 09:39:05 +0300

    wip

--- tests/Dashboard/SendMessageTest.php
+++ tests/Dashboard/SendMessageTest.php
@@ -2,10 +2,8 @@
 
 namespace BeyondCode\LaravelWebSockets\Tests\Dashboard;
 
-use BeyondCode\LaravelWebSockets\Statistics\Logger\MemoryStatisticsLogger;
-use BeyondCode\LaravelWebSockets\Tests\TestCase;
 use BeyondCode\LaravelWebSockets\Tests\Models\User;
-use BeyondCode\LaravelWebSockets\Tests\Mocks\Message;
+use BeyondCode\LaravelWebSockets\Tests\TestCase;
 
 class SendMessageTest extends TestCase
 {
@@ -28,6 +26,31 @@ class SendMessageTest extends TestCase
             ])
             ->seeJson([
                 'ok' => false,
+            ]);
+    }
+
+    /** @test */
+    public function can_send_message_on_redis_replication()
+    {
+        $this->skipOnLocalReplication();
+
+        // Because the Pusher server is not active,
+        // we expect it to turn out ok: false.
+        // However, the driver is set to redis,
+        // so Redis would take care of this
+        // and stream the message to all active servers instead.
+
+        $this->actingAs(factory(User::class)->create())
+            ->json('POST', route('laravel-websockets.event'), [
+                'appId' => '1234',
+                'key' => 'TestKey',
+                'secret' => 'TestSecret',
+                'channel' => 'test-channel',
+                'event' => 'some-event',
+                'data' => json_encode(['data' => 'yes']),
+            ])
+            ->seeJson([
+                'ok' => true,
             ]);
     }
 
